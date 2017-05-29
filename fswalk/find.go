0406f0c39df1a56b1f91920b03197a8f178dbd5e
ghst659 <ghst659@github.com>
2017-05-28 17:19:29 -0700

    added package to find files

--- fswalk/find.go
+++ fswalk/find.go
@@ -1,33 +1,14 @@
 package fswalk
 
 import (
-	"log"
 	"os"
 	"path/filepath"
 )
 
-type WalkMode int
-
-const (
-	NoWalk WalkMode = iota
-	Series
-	Parallel
-)
-
-func Files(root string, mode WalkMode, items chan<- string) {
-	walker = func(path string, info os.FileInfo, err error) error {
-		currentPath = filepath.Join(root, info.Name())
-		if info.IsDir() {
-			switch mode {
-			case Series:
-				Files(currentPath, mode, items)
-			case Parallel:
-				go Files(currentPath, mode, items)
-			default:
-				log.Printf("%s: no action", currentPath)
-			}
-		} else {
-			items <- currentPath
+func Files(root string, items chan<- string) {
+	walker := func(path string, info os.FileInfo, err error) error {
+		if ! info.IsDir() {
+			items <- path
 		}
 		return nil
 	}
--- fswalk/find_test.go
+++ fswalk/find_test.go
@@ -1,13 +1,17 @@
 package fswalk_test
 
-import "fmt"
-import "testing"
-import "github.com/ghst659/grepgo/fswalk"
+import (
+	"log"
+	"testing"
+	"github.com/ghst659/grepgo/fswalk"
+)
 
 func TestSequential(t *testing.T) {
 	ch := make(chan string)
-	go fswalk.Files("/tmp/foo", fswalk.Series, ch)
-	for i := range ch {
-		fmt.Println(i)
+	go fswalk.Files("/tmp/foo", ch)
+	i := 0
+	for item := range ch {
+		log.Printf("%d: %s", i, item)
+		i++
 	}
 }
