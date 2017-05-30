874b50a067ae36e014a9a85fd36ab009cfecbdba
ghst659 <ghst659@github.com>
2017-05-30 00:35:45 -0700

    Core routine cleanup.

--- grepper/core.go
+++ grepper/core.go
@@ -36,28 +36,27 @@ func yieldLoop(path string, filter func(string) bool, results chan<- SearchHit) 
 		defer file.Close()
 		source := bufio.NewReader(file)
 		for line_num := uint(0); err == nil; line_num++ {
-			var text string
-			if text, err = source.ReadString('\n'); err == nil && filter(text) {
+			if text, err := source.ReadString('\n'); err == nil && filter(text) {
 				results <- SearchHit{line_num, text}
+			} else if err == io.EOF {
+				break
 			}
 		}
 	}
 }
 
 func searchLoop(path string, filter func(string) bool) (result []SearchHit, err error) {
-	var file *os.File
-	if file, err = os.Open(path); err == nil {
+	if file, err := os.Open(path); err == nil {
 		defer file.Close()
 		source := bufio.NewReader(file)
 		for line_num := uint(0); err == nil; line_num++ {
-			var text string
-			if text, err = source.ReadString('\n'); err == io.EOF {
+			if text, err := source.ReadString('\n'); err == nil && filter(text) {
+				result = append(result, SearchHit{line_num, text})
+			} else if err == io.EOF {
 				err = nil
 				break
 			} else {
-				if filter(text) {
-					result = append(result, SearchHit{line_num, text})
-				}
+				// do nothing
 			}
 		}
 	}
